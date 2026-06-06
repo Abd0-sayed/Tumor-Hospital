1b2fd4dcd2788e868e49b8a88783423dd9644213
Abd0-sayed <habd.1234.habd@gmail.com>
2026-06-06 16:12:10 +0300

    refresh roken handler

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState,useEffect } from "react";
 
 const AuthContext = createContext();
 const getStorage = () => {
@@ -39,12 +39,77 @@ export function AuthProvider({ children }) {
 
     localStorage.removeItem("token");
     localStorage.removeItem("role");
+    localStorage.removeItem("refreshToken");
 
     setAuth({
       token: null,
       role: null,
     });
   };
+
+  let isRefreshing = false;
+
+async function refreshAccessToken() {
+  const getStorage = () => {
+  return sessionStorage.getItem("refreshToken")
+    ? sessionStorage
+    : localStorage;
+};
+  if (isRefreshing) return;
+
+  isRefreshing = true;
+
+  try {
+    const storage = getStorage();
+
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    if (!refreshToken) return;
+
+    const response = await fetch(
+      "https://tumorhospital.runasp.net/api/Auth/Refresh-Token",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          refreshToken,
+        }),
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error("Refresh token failed");
+    }
+
+    const data = await response.json();
+
+    storage.setItem("token", data.token);
+    storage.setItem("refreshToken", data.refreshToken);
+
+    setAuth((prev) => ({
+      ...prev,
+      token: data.token,
+    }));
+  } catch (error) {
+    console.error(error);
+
+    logout();
+  } finally {
+    isRefreshing = false;
+  }
+}
+
+useEffect(() => {
+  if (!auth.token) return;
+
+  const intervalId = setInterval(() => {
+    refreshAccessToken();
+  }, 30000);
+
+  return () => clearInterval(intervalId);
+}, [auth.token]);
 
   return (
     <AuthContext.Provider
